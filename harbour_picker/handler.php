5d7823bba3224ca1f43d26ea4f02f71c234cf762
Xavier Le Gal la Salle <xavier.lgls@gmail.com>
2019-10-03 01:31:16 +0200

    server side new harbour add handler

--- harbour_picker/handler.php
+++ harbour_picker/handler.php
@@ -1,2 +1,13 @@
 <?php
-echo "harbours handler";
+require("../config.php");
+require("../db.php");
+$db = new DB();
+if (isset($_POST["request"])) {
+    switch ($_POST["request"]) {
+        case "add":
+            if (isset($_POST["name"]) and isset($_POST["lat"]) and isset($_POST["lng"])) {
+                $db->insertANewHarbour($_POST["name"], $_POST["lat"], $_POST["lng"]);
+            }
+            break;
+    }
+}
