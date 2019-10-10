56ca5a87e13c4dcb9975b22b813811ed523a36a1
XavierLGLS <xavier.lgls@gmail.com>
2019-10-10 16:56:48 +0200

    code clean

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <title>CVE temporary website</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
